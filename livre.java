52aa774bb127361d62f7a0126cafbd076059e5ed
Iskander167 <147379669+Iskander167@users.noreply.github.com>
2023-10-09 10:00:32 +0100

    Add files via upload

---
+++ livre.java
@@ -0,0 +1,13 @@
+public class livre {
+    int id;
+    String titre,nom,prenom;
+    float prix;
+    public livre(int id,String titre,String nom,String prenom,float prix){
+        this.id = id;
+        this.titre = titre;
+        this.nom =nom;
+        this.prenom = prenom;
+        this.prix = prix;
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args){
+     private int id;
+     private String titre;
+     private String nom;
+     private String prenom;
+     private float prix;
+        livre l1 = new livre(150,"jerry","iskander","gachaoui", 4.500F);
+    }
+}
